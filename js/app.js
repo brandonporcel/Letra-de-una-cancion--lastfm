8435991ee5238632a5b74dd7ccdf90e7c1f32b0c
Familia Porcel <brandon7.7porcel@gmail.com>
2021-03-26 16:02:09 -0300

    validacion lista

--- js/app.js
+++ js/app.js
@@ -8,39 +8,74 @@ const $submitBtn = d.getElementById('submitBtn');
 const $lyrics = d.getElementById('lyrics');
 const LASTFM_KEY = 'f0ad510d53e346996bede562a182ab55';
 const LASTFM_ALBUM_URL = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${LASTFM_KEY}`;
-
+const NO_CONTENT_ERROR = 204;
+const noContentErrorText =
+	'Por favor revisa el casillero artista,cancion y album. Gracias';
 const $select = d.getElementById('info-select');
-const drawInfo = (data) => {};
 
-const getLyrics = async () => {
-	try {
-	} catch {}
-};
+const getLyrics = async () => {};
 const getAlbum = async () => {
 	try {
 		const artistName = $artistUserValue.value;
 		const songName = $songUserValue.value;
 		if ($select.value === 'album') {
+			// loader
 			const res = await fetch(
 				`${LASTFM_ALBUM_URL}&artist=${artistName}&album=${songName}&format=json`
 			);
 			const data = await res.json();
-			console.log(data);
+			if (
+				$select.value === 'undefined' ||
+				$artistUserValue.value === '' ||
+				$songUserValue.value === ''
+			) {
+				drawError({
+					status: `${NO_CONTENT_ERROR}`,
+					statusText: `${noContentErrorText}`,
+				});
+			} else if (data.error === 6 || data.links.length === 0) {
+				console.log(data);
+				throw { status: data.error, statusText: data.message };
+			}
 		}
-	} catch (error) {
-		console.log(error, 'error------------');
+	} catch (err) {
+		drawError(err);
 	}
 };
-const checkQuery = () => {
-	if ($select.value !== 'album') {
-		getLyrics();
-	} else {
-		getAlbum();
-	}
+const albumOrSongCheck = () => {
+	$form.addEventListener('submit', (e) => {
+		if (
+			$select.value === 'undefined' ||
+			$artistUserValue.value === '' ||
+			$songUserValue.value === ''
+		) {
+			drawError({
+				status: `${NO_CONTENT_ERROR}`,
+				statusText: `${noContentErrorText}`,
+			});
+		}
+		if ($select.value === 'album') {
+			getAlbum();
+		} else {
+			getLyrics();
+		}
+		e.preventDefault();
+	});
 };
-const drawError = () => {
-	console.log(
-		'hubo un error,revisa que hayas elegido un disco o que hayas puesto el artista'
+
+const drawError = (error) => {
+	if (d.getElementById('error-message')) {
+		d.getElementById('error-message').innerHTML = '';
+	}
+	const $errorMessage = d.createElement('h3');
+	$errorMessage.id = 'error-message';
+	let message =
+		error.statusText ||
+		'Ocurrio un error al buscar el disco. Es posible que no hayamos encontrado un resultado';
+	$errorMessage.innerHTML = `Error ${error.status} : ${message}`;
+	d.querySelector('.home-form').insertAdjacentElement(
+		'afterend',
+		$errorMessage
 	);
 };
 const disableFirstOption = () => {
@@ -48,14 +83,8 @@ const disableFirstOption = () => {
 		$select.firstElementChild.disabled = true;
 	});
 };
-$form.addEventListener('submit', (e) => {
-	if ($select.value === 'undefined' || $artistUserValue.value === '') {
-		drawError();
-	}
-	e.preventDefault();
-	getAlbum();
-});
 
 d.addEventListener('DOMContentLoaded', () => {
 	disableFirstOption();
+	albumOrSongCheck();
 });
